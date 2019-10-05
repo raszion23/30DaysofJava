45ea69000e9b43afea22483c4a7cacee2505c88f
Raszion23 <Raszion23@Saint-Francis.local>
2019-10-05 11:12:53 +0300

    initial commit

---
+++ src/pkg30daysofjava/Main.java
@@ -0,0 +1,15 @@
+/**
+ * File: Main.java
+ * Author: Anthony Francis
+ * Date: Oct 5, 2019
+ * Purpose:
+ */
+package pkg30daysofjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+      
+    }
+}
